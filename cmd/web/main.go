f2af159bc81cb19e71b0a4c68673f38a23f300dd
Asavir <supersingh@gmail.com>
2020-04-19 21:28:17 -0700

    adding config for db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -18,7 +18,7 @@ func main() {
 	cfg := config.ParseConfig()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-	db, err := openDB("root:root@tcp(localhost:33060)/authn?parseTime=true")
+	db, err := openDB(cfg.Dsn)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,6 +8,7 @@ import (
 type Config struct {
 	Addr      string
 	StaticDir string
+	Dsn       string
 }
 
 /*
@@ -26,6 +27,7 @@ func ParseConfig() Config {
 func (c *Config) parseFlags() {
 	flag.StringVar(&c.StaticDir, "static-dir", "", "Path to static assets")
 	flag.StringVar(&c.Addr, "addr", "", "HTTP network address, default is :4000")
+	flag.StringVar(&c.Addr, "dsn", "", "HTTP network address")
 	flag.Parse()
 }
 
@@ -34,7 +36,10 @@ func (c *Config) parseEnv() {
 		c.StaticDir = os.Getenv("QS_STATICDIR")
 	}
 	if isConfigBlank(c.Addr) {
-		c.StaticDir = os.Getenv("QS_ADDR")
+		c.Addr = os.Getenv("QS_ADDR")
+	}
+	if isConfigBlank(c.Dsn) {
+		c.Dsn = os.Getenv("QS_DSN")
 	}
 }
 
@@ -45,6 +50,10 @@ func (c *Config) defaults() {
 	if isConfigBlank(c.Addr) {
 		c.Addr = ":4000"
 	}
+	if isConfigBlank(c.Dsn) {
+		c.Dsn = "web:pass@/snippetbox?parseTime=true"
+	}
+
 }
 
 func isConfigBlank(s string) bool {
